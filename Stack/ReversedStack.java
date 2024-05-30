cc66fabcc7a27d39bfbb1132bb0c3b1c4d64a6bd
Kirill Kashirin <kirykaxxx@gmail.com>
2024-05-30 13:02:46 +0300

    Add Stack realisation

---
+++ Stack/ReversedStack.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class ReversedStack {
+    public List<Object> data;
+
+    public ReversedStack() {
+        data = new LinkedList<>();
+    }
+
+    public int size() {
+        return data.size();
+    }
+
+    public Object pop() {
+        if (data.isEmpty()) {
+            return null;
+        }
+        return data.removeFirst();
+    }
+
+    public void push(Object element) {
+        data.addFirst(element);
+    }
+
+    public Object peek() {
+        if (data.isEmpty()) {
+            return null;
+        }
+        return data.getFirst();
+    }
+}
+
+
---
+++ Stack/Stack.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Stack<T> {
+    public List<T> data;
+
+    public Stack() {
+        data = new LinkedList<T>();
+    }
+
+    public int size() {
+        return data.size();
+    }
+
+    public T pop() {
+        if (data.isEmpty()) {
+            return null;
+        }
+        return data.removeLast();
+    }
+
+    public void push(T val) {
+        data.addLast(val);
+    }
+
+    public T peek() {
+        if (data.isEmpty()) {
+            return null;
+        }
+        return data.getLast();
+    }
+}
+
+
