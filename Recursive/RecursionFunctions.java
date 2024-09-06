ea98eb24c9f2826afd8e8c6c214b9628af6205c3
Kirill Kashirin <kirykaxxx@gmail.com>
2024-09-06 12:39:06 +0300

    Add more recursive functions

--- Recursive/RecursionFunctions.java
+++ Recursive/RecursionFunctions.java
@@ -1,3 +1,10 @@
+package ru.ads.recursion;
+
+import ru.ads.stack.Stack;
+
+import java.util.Iterator;
+import java.util.List;
+
 public class RecursionFunctions {
 
     public static double powRecursive(double number, int power) {
@@ -23,4 +30,75 @@ public class RecursionFunctions {
         return (number % 10) + sumDigitsRecursive(number / 10);
     }
 
+    public static <T> int getListLengthRecursive(Stack<T> stack) {
+        if (stack.pop() == null) {
+            return 0;
+        }
+        return 1 + getListLengthRecursive(stack);
+    }
+
+    public static boolean isPalindromeRecursive(String sentence) {
+        if (sentence == null) {
+            return false;
+        }
+        String wordToCheck = sentence.trim().replace(" ", "").toLowerCase();
+        return isPalindromeRecursive(wordToCheck, 0, wordToCheck.length() - 1);
+    }
+
+    private static boolean isPalindromeRecursive(String word, int leftIndex, int rightIndex) {
+        if (leftIndex >= rightIndex) {
+            return true;
+        }
+        if (word.charAt(leftIndex) != word.charAt(rightIndex)) {
+            return false;
+        }
+        return isPalindromeRecursive(word, leftIndex + 1, rightIndex - 1);
+    }
+
+    public static <T extends Number> String getEvenValuesRecursive(List<T> list) {
+        if (list == null) {
+            return "";
+        }
+        return getEvenValuesRecursive(list.iterator()).trim();
+    }
+
+    private static <T extends Number> String getEvenValuesRecursive(Iterator<T> listIterator) {
+        if (!listIterator.hasNext()) {
+            return "";
+        }
+
+        T value = listIterator.next();
+        StringBuilder evenNumberStr = new StringBuilder();
+
+        if (isNumberEven(value)) {
+            evenNumberStr.append(value.intValue()).append(" ");
+        }
+        return evenNumberStr + getEvenValuesRecursive(listIterator);
+    }
+
+    private static boolean isNumberEven(Number value) {
+        return value.intValue() % 2 == 0;
+    }
+
+    public static <T> String getEvenIndexValuesRecursive(List<T> list) {
+        if (list == null) {
+            return "";
+        }
+        return getEvenIndexValuesRecursive(list, 0, list.size()).trim();
+    }
+
+    private static <T> String getEvenIndexValuesRecursive(List<T> list, int currentIndex, int endIndex) {
+        if (currentIndex == endIndex) {
+            return "";
+        }
+
+        StringBuilder evenIndexValueStr = new StringBuilder();
+
+        if (currentIndex % 2 == 0) {
+            evenIndexValueStr.append(list.get(currentIndex)).append(" ");
+        }
+        return evenIndexValueStr + getEvenIndexValuesRecursive(list, currentIndex + 1, endIndex);
+    }
+
 }
+
