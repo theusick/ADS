bf0c309041c455609bea1eca00af5945215d6983
Kirill Kashirin <kirykaxxx@gmail.com>
2024-09-18 23:57:54 +0300

    Update jdk version to 22, add .editorconfig, add .vscode into .gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        languageVersion.set(JavaLanguageVersion.of(22))
     }
 }
 
--- src/test/java/ru/ads/deque/DequeTest.java
+++ src/test/java/ru/ads/deque/DequeTest.java
@@ -1,6 +1,5 @@
 package ru.ads.deque;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
