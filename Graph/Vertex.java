31394f71c33a009129b0556928c82ffacea07845
Kirill Kashirin <kirykaxxx@gmail.com>
2024-10-14 23:09:46 +0300

    Add SimpleGraph realization

---
+++ Graph/Vertex.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+class Vertex {
+    public int Value;
+
+    public Vertex(int val) {
+        Value = val;
+    }
+
+}
+
+class SimpleGraph {
+    Vertex[] vertex;
+    int[][] m_adjacency;
+    int max_vertex;
+    int currentSize = 0;
+
+    public SimpleGraph(int size) {
+        max_vertex = size;
+        m_adjacency = new int[size][size];
+        vertex = new Vertex[size];
+    }
+
+    public void AddVertex(int value) {
+        if (currentSize < max_vertex) {
+            vertex[currentSize] = new Vertex(value);
+            currentSize++;
+        }
+    }
+
+    public void RemoveVertex(int v) {
+        if (IndexOutOfBounds(v)) {
+            return;
+        }
+
+        for (int i = 0; i < max_vertex; i++) {
+            m_adjacency[v][i] = 0;
+            m_adjacency[i][v] = 0;
+        }
+        vertex[v] = null;
+        currentSize--;
+    }
+
+    public boolean IsEdge(int v1, int v2) {
+        if (OneIsOutOfBounds(v1, v2)) {
+            return false;
+        }
+        return (m_adjacency[v1][v2] == 1) || (m_adjacency[v2][v1] == 1);
+    }
+
+    private boolean IndexOutOfBounds(int index) {
+        return (index < 0) || (index >= max_vertex);
+    }
+
+    private boolean OneIsOutOfBounds(int v1, int v2) {
+        return (IndexOutOfBounds(v1) || IndexOutOfBounds(v2));
+    }
+
+    public void AddEdge(int v1, int v2) {
+        if (OneIsOutOfBounds(v1, v2)) {
+            return;
+        }
+        m_adjacency[v1][v2] = 1;
+        m_adjacency[v2][v1] = 1;
+    }
+
+    public void RemoveEdge(int v1, int v2) {
+        if (OneIsOutOfBounds(v1, v2)) {
+            return;
+        }
+        m_adjacency[v1][v2] = 0;
+        m_adjacency[v2][v1] = 0;
+    }
+
+}
