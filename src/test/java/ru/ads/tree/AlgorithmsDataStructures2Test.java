46ad16f436a1c8902adbdd7543cb16a80e1b18b6
Kirill Kashirin <kirykaxxx@gmail.com>
2024-10-15 02:35:24 +0300

    Add array AVL deletion and fix IsBalanced in BSTNode

--- src/test/java/ru/ads/tree/AlgorithmsDataStructures2Test.java
+++ src/test/java/ru/ads/tree/AlgorithmsDataStructures2Test.java
@@ -50,4 +50,140 @@ class AlgorithmsDataStructures2Test {
         assertArrayEquals(expected, result);
     }
 
+    @Test
+    public void testIsBBSTEmptyTree() {
+        Integer[] emptyTree = {};
+        assertTrue(AlgorithmsDataStructures2.IsBBST(emptyTree));
+    }
+
+    @Test
+    public void testIsBBSTSingleNode() {
+        Integer[] singleNodeTree = {10};
+        assertTrue(AlgorithmsDataStructures2.IsBBST(singleNodeTree));
+    }
+
+    @Test
+    public void testIsBBSTBalancedTree() {
+        Integer[] balancedTree = {10, 5, 15, 3, 7, 13, 17};
+        assertTrue(AlgorithmsDataStructures2.IsBBST(balancedTree));
+    }
+
+    @Test
+    public void testIsBBSTUnbalancedLeftTree() {
+        Integer[] leftHeavyTree = {10, 5, null, 3, null, null, null};
+        assertFalse(AlgorithmsDataStructures2.IsBBST(leftHeavyTree));
+    }
+
+    @Test
+    public void testIsBBSTUnbalancedRightTree() {
+        Integer[] rightHeavyTree = {10, null, 15, null, null, null, 17};
+        assertFalse(AlgorithmsDataStructures2.IsBBST(rightHeavyTree));
+    }
+
+    @Test
+    public void testIsBBSTNullNodeInTree() {
+        Integer[] treeWithNull = {10, 5, null, 3, null, 6, null};
+        assertFalse(AlgorithmsDataStructures2.IsBBST(treeWithNull));
+    }
+
+    @Test
+    public void testDeleteNodeBBSTEmptyArray() {
+        Integer[] input = {};
+
+        assertFalse(AlgorithmsDataStructures2.DeleteNodeBBST(input, 5));
+        assertTrue(AlgorithmsDataStructures2.IsBBST(input));
+    }
+
+    @Test
+    public void testDeleteNodeBBSTSingleElement() {
+        Integer[] input = {5};
+        Integer[] expected = {null};
+
+        assertTrue(AlgorithmsDataStructures2.DeleteNodeBBST(input, 5));
+        assertArrayEquals(expected, input);
+        assertTrue(AlgorithmsDataStructures2.IsBBST(input));
+    }
+
+    @Test
+    public void testDeleteNodeBBSTDepth2() {
+        Integer[] input = {3, 1, 4};
+        Integer[] expected = {3, null, 4};
+
+        assertTrue(AlgorithmsDataStructures2.DeleteNodeBBST(input, 1));
+        assertArrayEquals(expected, input);
+        assertTrue(AlgorithmsDataStructures2.IsBBST(input));
+    }
+
+    @Test
+    public void testDeleteNodeBBSTDepth4() {
+        Integer[] input = {7, 3, 11, 1, 5, 9, 13, 0, 2, 4, 6, 8, 10, 12, 14};
+        Integer[] expected = {7, 3, 10, 1, 5, 9, 13, 0, 2, 4, 6, 8, null, 12, 14};
+
+        assertTrue(AlgorithmsDataStructures2.DeleteNodeBBST(input, 11));
+        assertArrayEquals(expected, input);
+        assertTrue(AlgorithmsDataStructures2.IsBBST(input));
+
+        expected = new Integer[]{7, 3, 10, 1, 5, 9, 12, 0, 2, 4, 6, 8, null, null, 14};
+
+        assertTrue(AlgorithmsDataStructures2.DeleteNodeBBST(input, 13));
+        assertArrayEquals(expected, input);
+        assertTrue(AlgorithmsDataStructures2.IsBBST(input));
+
+        expected = new Integer[]{7, 3, 10, 1, 5, 9, 12, 0, 2, 4, 6, 8, null, null, null};
+
+        assertTrue(AlgorithmsDataStructures2.DeleteNodeBBST(input, 14));
+        assertArrayEquals(expected, input);
+        assertTrue(AlgorithmsDataStructures2.IsBBST(input));
+    }
+
+    @Test
+    public void testDeleteNodeLeftRotation() {
+        Integer[] input = {30, 20, 40, 10, 25, 35, 50};
+        Integer[] expected = {30, 10, 40, null, 25, 35, 50};
+
+        assertTrue(AlgorithmsDataStructures2.DeleteNodeBBST(input, 20));
+        assertArrayEquals(expected, input);
+        assertTrue(AlgorithmsDataStructures2.IsBBST(input));
+    }
+
+    @Test
+    public void testDeleteNodeRightRotation() {
+        Integer[] input = {30, 20, 40, 10, null, 35, 50};
+        Integer[] expected = {30, 10, 40, null, null, 35, 50};
+
+        assertTrue(AlgorithmsDataStructures2.DeleteNodeBBST(input, 20));
+        assertArrayEquals(expected, input);
+        assertTrue(AlgorithmsDataStructures2.IsBBST(input));
+    }
+
+    @Test
+    public void testDeleteNodeLeftRightRotation() {
+        Integer[] input = {30, 10, 40, null, 20, 35, 50};
+        Integer[] expected = {30, 20, 40, null, null, 35, 50};
+
+        assertTrue(AlgorithmsDataStructures2.DeleteNodeBBST(input, 10));
+        assertArrayEquals(expected, input);
+        assertTrue(AlgorithmsDataStructures2.IsBBST(input));
+    }
+
+    @Test
+    public void testDeleteNodeRightLeftRotation() {
+        Integer[] input = {30, 10, 40, 5, 20, 35, 50};
+        Integer[] expected = {30, 10, 35, 5, 20, null, 50};
+
+        assertTrue(AlgorithmsDataStructures2.DeleteNodeBBST(input, 40));
+        assertArrayEquals(expected, input);
+        assertTrue(AlgorithmsDataStructures2.IsBBST(input));
+    }
+
+    @Test
+    public void testDeleteNodeBBSTNoGapsBeforeDeletion() {
+        Integer[] input = {7, 3, 11, 1, 5, 9, 13, 0, 2, 4, 6, 8, 10, 12, 14};
+        Integer[] expected = {7, 3, 11, 1, 5, 9, 12, 0, 2, 4, 6, 8, 10, null, 14};
+
+        assertTrue(AlgorithmsDataStructures2.DeleteNodeBBST(input, 13));
+        assertArrayEquals(expected, input);
+        assertTrue(AlgorithmsDataStructures2.IsBBST(input));
+    }
+
 }
