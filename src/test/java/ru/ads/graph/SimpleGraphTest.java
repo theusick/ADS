d5da73df2d8a9b520e1fa8cf0e2320fc5cd65a8a
Kirill Kashirin <kirykaxxx@gmail.com>
2024-10-29 20:23:43 +0300

    Add BreadthFirstSearch for SimpleGraph realization

--- src/test/java/ru/ads/graph/SimpleGraphTest.java
+++ src/test/java/ru/ads/graph/SimpleGraphTest.java
@@ -252,4 +252,92 @@ class SimpleGraphTest {
         assertFalse(largeGraph.IsGraphConnected());
     }
 
+    @Test
+    void testBreadthFirstSearchSmallGraph() {
+        List<Vertex> path = smallGraph.BreadthFirstSearch(0, 1);
+        assertTrue(path.isEmpty());
+
+        smallGraph.AddEdge(0, 1);
+
+        path = smallGraph.BreadthFirstSearch(0, 1);
+        assertEquals(2, path.size());
+        assertEquals(1, path.get(0).Value);
+        assertEquals(2, path.get(1).Value);
+
+        path = smallGraph.BreadthFirstSearch(1, 0);
+        assertEquals(2, path.size());
+        assertEquals(2, path.get(0).Value);
+        assertEquals(1, path.get(1).Value);
+
+        path = smallGraph.BreadthFirstSearch(0, 2);
+        assertTrue(path.isEmpty());
+    }
+
+    @Test
+    void testBreadthFirstSearchMediumGraph() {
+        mediumGraph.AddEdge(0, 1);
+        mediumGraph.AddEdge(1, 2);
+        mediumGraph.AddEdge(1, 3);
+
+        List<Vertex> path = mediumGraph.BreadthFirstSearch(0, 3);
+        assertEquals(3, path.size());
+        assertEquals(1, path.get(0).Value);
+        assertEquals(2, path.get(1).Value);
+        assertEquals(4, path.get(2).Value);
+
+        path = mediumGraph.BreadthFirstSearch(3, 0);
+        assertEquals(3, path.size());
+        assertEquals(4, path.get(0).Value);
+        assertEquals(2, path.get(1).Value);
+        assertEquals(1, path.get(2).Value);
+
+        path = mediumGraph.BreadthFirstSearch(0, 4);
+        assertTrue(path.isEmpty());
+    }
+
+    @Test
+    void testBreadthFirstSearchLargeGraph() {
+        largeGraph.AddEdge(0, 1);
+        largeGraph.AddEdge(0, 2);
+        largeGraph.AddEdge(1, 3);
+        largeGraph.AddEdge(2, 4);
+        largeGraph.AddEdge(3, 5);
+        largeGraph.AddEdge(4, 5);
+        largeGraph.AddEdge(5, 6);
+        largeGraph.AddEdge(6, 7);
+        largeGraph.AddEdge(7, 8);
+
+        List<Vertex> path = largeGraph.BreadthFirstSearch(0, 8);
+        assertEquals(7, path.size());
+        assertEquals(1, path.getFirst().Value);
+        assertEquals(2, path.get(1).Value);
+        assertEquals(4, path.get(2).Value);
+        assertEquals(6, path.get(3).Value);
+        assertEquals(7, path.get(4).Value);
+        assertEquals(8, path.get(5).Value);
+        assertEquals(9, path.getLast().Value);
+
+        path = largeGraph.BreadthFirstSearch(8, 0);
+        assertEquals(7, path.size());
+
+        path = largeGraph.BreadthFirstSearch(2, 6);
+        assertEquals(4, path.size());
+
+        path = largeGraph.BreadthFirstSearch(0, 7);
+        assertEquals(6, path.size());
+    }
+
+    @Test
+    void testBreadthFirstSearchDisconnectedGraph() {
+        mediumGraph.AddEdge(0, 1);
+        mediumGraph.AddEdge(2, 3);
+
+        List<Vertex> path = mediumGraph.BreadthFirstSearch(0, 3);
+        assertTrue(path.isEmpty());
+
+        path = mediumGraph.BreadthFirstSearch(1, 2);
+        assertTrue(path.isEmpty());
+    }
+
+
 }
